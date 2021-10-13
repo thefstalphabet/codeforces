ef94a4d5fc631cf3a64bbcd4da4bab6f38399f69
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2021-10-13 21:26:19 +0530

    Add files via upload

---
+++ practice solutions/Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve(){
+	// write yor code here
+    ll w;
+    cin>>w;
+    if(w%2==0 && w!=2) cout<<"YES"<<"\n";
+    else cout<<"NO"<<"\n";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
