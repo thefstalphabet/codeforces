6ce52003a2cae9fadc44ed7d1414374ed0d6d62c
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2022-03-11 13:13:30 +0530

    Add files via upload

---
+++ educational round/124 (division 2)/Playoff.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve(){
+    int n; cin>>n;
+    cout<<(1<<n)-1<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
